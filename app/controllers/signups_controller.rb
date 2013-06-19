a7f6698ab27668261f30dc2d701a258894d65e59
Tenzin Nyima <tenzin@berkeley.edu>
2013-06-18 19:37:23 -0700

    added more input fields for signup form, validation does some basic validations and input field retain values except password

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -14,35 +14,86 @@ class SignupsController < ApplicationController
   end
 
   def signUp
+    @company_name = params[:company_name]
   	@id = params[:username]
     @pw = params[:password]
+    @email = params[:email]
+    @phone = params[:phone]
+    session[:company_name] = @company_name
+    session[:username] = @id
+    session[:email] = @email
+    session[:phone] = @phone
 
-    if validate(@id, @pwd)
-	    @url = "https://208.65.111.144/rest/Account/add_account/{'session_id':'#{@@session_id}'}/{'account_info':{'i_customer':'1552','i_product':'1','activation_date':'2009-2-23','id':'#{@id}','balance':'0','opening_balance':'0','login':'#{@id}','h323_password':'#{@pw}','blocked':'Y'}}"
-	    @uri = uriEncoder(@url)
-	    @response = RestClient::Request.new(
-	      :method => :post,
-	      :url => @uri,
-	      :headers => { :accept => :json, :content_type => :json}).execute
+    if validate_id(@id)
+      if validate_pw(@pw)
+        if validate_email(@email)
+          if validate_phone(@phone)
+	          @url = "https://208.65.111.144/rest/Account/add_account/{'session_id':'#{@@session_id}'}/{'account_info':{'i_customer':'1552','i_product':'1','activation_date':'2009-2-23','id':'#{@id}','balance':'0','opening_balance':'0','login':'#{@id}','h323_password':'#{@pw}','blocked':'Y', 'companyname':'#{@company_name}','phone1':'#{@phone}' ,'subscriber_email':'#{@email}'}}"
+	          @uri = uriEncoder(@url)
+	          @response = RestClient::Request.new(
+	            :method => :post,
+	            :url => @uri,
+	            :headers => { :accept => :json, :content_type => :json}).execute
 
-	    @result = ActiveSupport::JSON.decode(@response)
+	          @result = ActiveSupport::JSON.decode(@response)
+          else
+            flash[:error] = "Phone Number is not valid"
+            redirect_to "/"
+          end
+        else
+          flash[:error] = "Email is not valid"
+          redirect_to "/"
+        end
+      else 
+        flash[:error] = "Password cannot have fewer than 6 characters"
+        redirect_to "/"
+      end
 	  else 
-  	  flash[:error] = "Your username and password cannot have fewer than 6 characters."
+  	  flash[:error] = "Username cannot have fewer than 6 characters"
   	  redirect_to "/"
-	end
+	  end
   end
+
+  ###### HELPER METHOFS ######
 
   #URI enencoder helper method
   def uriEncoder (uri)
     return URI.encode(uri.gsub!("'", '"'))
   end
 
-  def validate(id,pw)
-  	if id.length() >= 6 and pw.length >= 6
+  # Validation helper method
+  def validate_id(id)
+  	if id.length() >= 6
   		return true
   	else 
   		return false
   	end
+  end
+
+  def validate_pw(pw)
+    if pw.length() >= 6
+      return true
+    else 
+      return false
+    end
+  end
+
+  def validate_email(email)
+    #We should validate the email with regex 
+    if email.length() >= 6
+      return true
+    else 
+      return false
+    end
+  end
+
+  def validate_phone(phone)
+    #We should validate the email with regex 
+    if phone.length() == 10
+      return true
+    else 
+      return false
+    end
   end
 
 end
