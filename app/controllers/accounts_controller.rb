a4b8a809f4a4aa666b7032a8c3950d7b999192de
Tenzin Nyima <tenzin@berkeley.edu>
2013-06-21 13:16:52 -0700

    authentication without using model

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,14 +1,18 @@
 class AccountsController < ApplicationController
   def index
-    puts session[:session_id]
+    if not session[:current_login]
+      redirect_to root_path
+    end
   end
 
   def account_list
-    if session[:current_user_id]
-      @url = "https://208.65.111.144:8444/rest/Account/get_account_list/%7B%22session_id%22:%22377edf68679d89c0c2fd8dd098721778%22%7D/%7B%22offset%22:%2220%22,%20%22i_customer%22:%221552%22%7D"
+    if session[:current_login]
+      @session_id = get_session
+      @url = "https://208.65.111.144/rest/Account/get_account_list/{'session_id':'#{@session_id}'}/{'i_customer':'1552'}"
+      @uri = uriEncoder(@url)
       @response = RestClient::Request.new(
         :method => :post,
-        :url => @url,
+        :url => @uri,
         :headers => { :accept => :json, :content_type => :json}).execute
       @result = ActiveSupport::JSON.decode(@response)
     else
@@ -18,7 +22,7 @@ class AccountsController < ApplicationController
   end
 
   def update_account
-    if session[:current_user_id]
+    if session[:current_login]
       @url = "https://208.65.111.144:8444/rest/Account/get_account_list/%7B%22session_id%22:%22377edf68679d89c0c2fd8dd098721778%22%7D/%7B%22offset%22:%2220%22,%20%22i_customer%22:%221552%22%7D"
       @response = RestClient::Request.new(
         :method => :post,
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,23 +7,45 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	account = Account.authenticate(params[:username], params[:password])
+    @login = params[:username]
+    @pw = params[:password]
 
-  	if account
-      session[:session_id] = get_session
-      session[:current_user_id] = account["login"]
-      session[:password] = account["password"]
+    @session_id = get_session
+
+    @url = "https://208.65.111.144/rest/Account/get_account_list/{'session_id':'#{@session_id}'}/{'i_customer':'1552'}"
+    @uri = uriEncoder(@url)
+    puts @uri
+
+    @response = RestClient::Request.new(
+      :method => :post,
+      :url => @uri,
+      :headers => { :accept => :json, :content_type => :json}).execute
+    @result = ActiveSupport::JSON.decode(@response)
+
+    @result["account_list"].each do |account|
+      puts account["password"]
+      if account["login"] == @login
+        if account["password"] == @pw
+          session[:current_login] = @login
+          session[:current_pw] = @pw
+        end
+      end
+    end
+
+    if session[:current_login]
       flash[:notice] = "You are successfuly logged in!"
       redirect_to '/accounts'
-    else
+    else 
       flash[:error] = "Wrong Login or Password!"
       redirect_to '/sessions/new'
     end
+
   end
 
   def destroy
-  	reset_session
+    reset_session
     flash[:notice] = "You are successfuly logged out!"
     redirect_to '/sessions/new'
   end
+  	
 end
