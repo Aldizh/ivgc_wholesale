4d85b24bcf7bd9dcefcb952a765bed2d1afd915a
Tenzin Nyima <tenzin@berkeley.edu>
2013-07-11 14:10:56 -0700

    i_account is now dynamic

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -141,7 +141,7 @@ class AccountsController < ApplicationController
     else
       @payment_amount = (details.params["PaymentDetails"]["OrderTotal"]).to_i || 0
       response = EXPRESS_GATEWAY.purchase(@payment_amount*100, {:ip => getIP, :token => session[:token], :payer_id => details.payer_id})
-      @url = "https://208.65.111.144/rest/Account/make_transaction/{'session_id':'9dd4eccdcd7b97039fc6ce95e1a68b9f'}/{'i_account':'877815', 'amount':'1', 'action':'Manual Payment', 'visible_comment':'test payment', 'internal_comment':'Not Available', 'suppress_notification':'1'}"
+      @url = "https://208.65.111.144/rest/Account/make_transaction/{'session_id':'9dd4eccdcd7b97039fc6ce95e1a68b9f'}/{'i_account':'#{session[:i_account]}', 'amount':'1', 'action':'Manual Payment', 'visible_comment':'test payment', 'internal_comment':'Not Available', 'suppress_notification':'1'}"
       apiRequest(@url)
       flash[:notice] = "$#{@payment_amount}" + " was added to your account!"
     end
