13b72c8c34b7e1cbd944d42a699a821379825511
Aldi Zhupani <aldizhupani@gmail.com>
2013-07-15 12:33:51 -0700

    Finally switching to mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130715163937) do
 
-  create_table "accounts", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "tickets", :force => true do |t|
     t.string   "title"
     t.text     "body"
