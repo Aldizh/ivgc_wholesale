ea8434a24d320ec90340e0dde9f17b957c2f6c53
Tenzin Nyima <tenzin@berkeley.edu>
2013-07-24 14:51:29 -0700

    rates continued

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,9 +1,12 @@
 class RatesController < ApplicationController
   def index
-  	@rates = Rate.all
   end
   def import
     Rate.import(params[:file])
 	redirect_to rates_url, notice: "Rates imported."
+  end
+  def displayRate 
+  	@rates = Rate.all
+  	render :layout => false
   end
 end
