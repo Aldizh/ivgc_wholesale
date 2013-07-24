53b84cbb3fc727c26d13077b4c2df241be709dff
Aldi Zhupani <aldizhupani@gmail.com>
2013-07-24 15:50:20 -0700

    added check for rates

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -3,11 +3,9 @@ class RatesController < ApplicationController
   end
   def import
   	CSV.foreach(params[:file].path, headers: true) do |row|
-	    rate = Rate.find_by_id(row["id"]) || Rate.new
-	    if not rate
-	    	rate.attributes = row.to_hash.slice(*Rate.accessible_attributes)
-	    	rate.save!
-	    end
+	    rate = Rate.find_by_country(row["country"]) || Rate.new
+	    rate.attributes = row.to_hash.slice(*Rate.accessible_attributes)
+	    rate.save!
   	end
 	redirect_to rates_url, notice: "Rates imported."
   end
