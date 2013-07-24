d45ec8c8f07b402eab95868a98d9873b88ce8b5d
Aldi Zhupani <aldizhupani@gmail.com>
2013-07-24 14:56:55 -0700

    Adding check to avoid duplicates

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -3,7 +3,14 @@ class RatesController < ApplicationController
   	@rates = Rate.all
   end
   def import
-    Rate.import(params[:file])
+  	spreadsheet = Rate.import(params[:file])
+  	header = spreadsheet.row(1)
+  	(2..spreadsheet.last_row).each do |i|
+	    row = Hash[[header, spreadsheet.row(i)].transpose]
+	    product = find_by_id(row["id"]) || new
+	    product.attributes = row.to_hash.slice(*accessible_attributes)
+	    product.save!
+  	end
 	redirect_to rates_url, notice: "Rates imported."
   end
 end
