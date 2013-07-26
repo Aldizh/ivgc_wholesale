318f380de04d75c3d792020cbec221a1a684be08
Aldi Zhupani <aldizhupani@gmail.com>
2013-07-25 17:37:14 -0700

    Adding filtering for rates alphabetically

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -9,8 +9,29 @@ class RatesController < ApplicationController
   	end
 	redirect_to rates_url, notice: "Rates imported."
   end
-  def displayRate 
-  	@rates = Rate.all
-  	render :layout => false
+  def displayRate
+  	#@rates = Rate.all
+    @url = "https://208.65.111.144/rest/Rate/get_rate_list/{'session_id':'#{get_session2}'}/{'i_tariff':'72', 'effective_from':'now', 'limit':'100'}"
+    @result = apiRequest(@url)
+    if params[:first_letter] and params[:first_letter] != '*'
+      url_all = "https://208.65.111.144/rest/Rate/get_rate_list/{'session_id':'#{get_session2}'}/{'i_tariff':'72', 'effective_from':'now'}"
+      result_all = apiRequest(url_all)
+      all_rates = result_all['rate_list']
+      @rates = []
+      all_rates.each do |r|
+        if r['country'].start_with?("#{params[:first_letter]}")
+          @rates.push(r)
+        end
+      end
+      render :layout => false
+    elsif params[:first_letter] == '*'
+      url_all = "https://208.65.111.144/rest/Rate/get_rate_list/{'session_id':'#{get_session2}'}/{'i_tariff':'72', 'effective_from':'now'}"
+      result = apiRequest(url_all)
+      @rates = result['rate_list']
+      render :layout => false
+    else
+      @rates = @result['rate_list']
+  	  render :layout => false
+    end
   end
 end
